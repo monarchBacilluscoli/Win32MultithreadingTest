b44176e1556fd83cc529145c3ef5cf1d74c014ee
LiuYongfeng <531817981@qq.com>
2021-02-01 23:03:58 +0800

    "+"

---
+++ MemoryMappedFileTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+using System.Collections;
+
+namespace MemoryMappedFileTest
+{
+    struct SomeValue
+    {
+        public SomeValue(int ra, char rc) //? 忘了类如果实现接口，那么调用接口成员会不会装箱了？
+        {
+            a = ra;
+            c = rc;
+        }
+        public int a;
+        public char c;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryMappedFile sharedMemory = MemoryMappedFile.CreateNew("TestSharedMemoryFIle", 1000);
+            MemoryMappedViewAccessor sharedMemoryAccessor = sharedMemory.CreateViewAccessor();
+
+            SomeValue sv1 = new SomeValue(1, 'r'), sv2;
+
+            sharedMemoryAccessor.Write(0, ref sv1);
+            sharedMemoryAccessor.Read(0, out sv2);
+
+            while (true)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MemoryMappedFileTest2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+using System.Collections;
+
+namespace MemoryMappedFileTest
+{
+    struct SomeValue
+    {
+        public SomeValue(int ra, char rc) //? 忘了类如果实现接口，那么调用接口成员会不会装箱了？
+        {
+            a = ra;
+            c = rc;
+        }
+        public int a;
+        public char c;
+        public override string ToString()
+        {
+            return String.Format("a: {0}, c: {1}", a, c);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryMappedViewAccessor sharedMemoryAccessor;
+            try
+            {
+                MemoryMappedFile sharedMemory = MemoryMappedFile.OpenExisting("TestSharedMemoryFIle");
+                sharedMemoryAccessor = sharedMemory.CreateViewAccessor();
+
+                SomeValue sv2;
+                sharedMemoryAccessor.Read(0, out sv2);
+
+                // sharedMemoryAccessor.Write(0, ref sv1);
+                Console.WriteLine(sv2);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
