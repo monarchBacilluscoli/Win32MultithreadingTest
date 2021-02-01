52fa8510ec6da64e376bbb749202c3a05d8fc7fe
liuyongfeng <531817981@qq.com>
2021-02-01 21:41:48 +0800

    + wrapped rwlock, +MMF

---
+++ MemoryMappedFileTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+
+namespace MemoryMappedFileTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryMappedFile sharedMemory = MemoryMappedFile.CreateNew("TestSharedMemoryFIle", 1000);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
